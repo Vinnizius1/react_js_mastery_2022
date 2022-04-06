756abd20bd093de418ab3bc6463ee8ee04c5eafb
Vinicius Matos de Mendonça <concursandopro1@gmail.com>
2022-04-06 12:18:19 -0300

    início do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import React from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    /* NUNCA MODIFIQUE "STATE" MANUALMENTE! 
-      counter = 100;
-      ERRADO!
-      O "react state" somente pode ser mudado usando a sua própria função "setState",
-      no nosso caso "setCounter"!
-    */
-  }, [])
-
   return (
-    <div className="App">
-      <button onClick={() => {setCounter((prevCount) => prevCount - 1)}}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => {setCounter((prevCount) => prevCount + 1)}}>+</button>
-    </div>
+    <>
+      <h1>App</h1>
+    </>
   );
 };
 
