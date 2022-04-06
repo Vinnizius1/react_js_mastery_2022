fc93dce7f60e4d38499fdd93ada85aa1297684cd
Vinicius Matos de Mendonça <concursandopro1@gmail.com>
2022-04-06 13:12:55 -0300

    fetch da api de filmes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
+
+// Here is your key: b1639244
+
+const API_URL = "http://www.omdbapi.com?apikey=b1639244";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+      searchMovies("batman")
+  }, []);
+
   return (
     <>
       <h1>App</h1>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// Após a atualização React 18 ficou deste jeito:
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
